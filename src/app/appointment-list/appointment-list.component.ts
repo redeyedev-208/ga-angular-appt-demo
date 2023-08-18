eb7ae4ea321b737b04b4469a13af48bc1350cddc
gilaguilar <redeyedev.208@gmail.com>
2023-08-18 13:17:44 -0600

    feat: create interface for one way data binding example

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Appointment } from '../models/appointment';
 
 @Component({
   selector: 'app-appointment-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./appointment-list.component.css']
 })
 export class AppointmentListComponent {
-  appointment: string = "Go buy some more coffee beans"
+  appointment: Appointment = {
+    id: 1,
+    title: "Go get some new coffee beans",
+    date: new Date('2023-08-18')
+  }
 }
