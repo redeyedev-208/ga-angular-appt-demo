e09486cbb975bf1f61757a4997eaab7868593882
gilaguilar <redeyedev.208@gmail.com>
2023-08-18 14:33:28 -0600

    feat: added local storage and tested logic

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css'],
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit {
   newAppointmentTitle: string = '';
   newAppointmentDate: Date = new Date();
 
   appointments: Appointment[] = [];
+
+  // We need to create an interface with this lifecycle hook
+  ngOnInit(): void {
+    let savedAppointments = localStorage.getItem('appointments');
+
+    this.appointments = savedAppointments ? JSON.parse(savedAppointments) : [];
+  }
 
   addAppointment() {
     // Check only if we have entered a title and a date so that it is a valid appointment
@@ -24,10 +32,13 @@ export class AppointmentListComponent {
 
       this.newAppointmentTitle = '';
       this.newAppointmentDate = new Date();
+
+      localStorage.setItem('appointments', JSON.stringify(this.appointments));
     }
   }
 
   deleteAppointment(index: number) {
     this.appointments.splice(index, 1);
+    localStorage.setItem('appointments', JSON.stringify(this.appointments));
   }
 }
